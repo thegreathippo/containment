59573ecf2a999318e741eaec9e1d642064565d72
thegreathippo <thegreathippo@gmail.com>
2017-12-03 22:44:15 -0500

    Some basic groundwork.

--- containment/game/core.py
+++ containment/game/core.py
@@ -7,10 +7,13 @@ from kvy import Clock
 
 class GameApp(App):
 
+    _speed = 1
     session = None
     date = StringProperty()
     time = StringProperty()
-    stats = StringProperty()
+    fs = StringProperty()  # foundation status
+    ws = StringProperty()  # world status
+    title = "Containment: Apocalypse"
 
     def build(self):
         window = MainWindow()
@@ -18,23 +21,43 @@ class GameApp(App):
 
     def new_game(self):
         self.session = Session()
-        Clock.schedule_interval(self.tick, 0.1)
-
-    def add_funds(self):
-        self.session.funds += 100
+        Clock.schedule_interval(self.tick, 0.05)
 
     def tick(self, *args):
-        self.session.tick()
-        self.time = self.session.time
-        self.date = self.session.date
-        self.update_stats()
+        if self._speed:
+            self.session.tick()
+            self.time = self.session.time
+            self.date = self.session.date
+        self.update_status()
 
-    def update_stats(self):
-        funds = "$" + str(self.session.funds) + "k"
-        self.stats = " SITES:     {0}\n FUNDS:     {1}\n ".format(1, funds)
+    def update_status(self):
+        self.fs = _foundation_status(self.session.foundation)
+        self.ws = _world_status(self.session.world)
+
+    def pause(self):
+        self._speed = 0
+
+    def resume(self):
+        self._speed = 1
 
 
 class MainWindow(ScreenManager):
     pass
 
 
+def _foundation_status(foundation):
+    agent = " PERSONNEL:  {0}".format(foundation.personnel)
+    sites = " SITES:      {0}".format(foundation.sites)
+    funds = " FUNDS:      ${0}k".format(foundation.funds)
+    anoma = " ANOMALIES:  {0}/{1}".format(foundation.anomalies, foundation.max_anomalies)
+    resea = " RESEARCH:   {0}".format(foundation.research)
+    data = agent + "\n" + sites + "\n" + anoma + "\n" + funds + "\n" + resea
+    return data
+
+
+def _world_status(world):
+    popul = " POPULATION: {0}".format(world.population)
+    anoma = " ANOMALIES:  {0}".format(world.anomalies)
+    panic = " PANIC:      {0}".format(world.panic)
+    data = popul + "\n" + anoma + "\n" + panic
+    return data
--- containment/game/session.py
+++ containment/game/session.py
@@ -1,6 +1,25 @@
 M_31 = 1, 3, 5, 7, 8, 10, 12
 M_30 = 4, 6, 9, 11
 
+#  FOUNDATION
+SITES = 1
+FUNDS = 10
+PERSONNEL = 0
+FOUNDATION_ANOMALIES = 0
+RESEARCH = 0
+
+#  WORLD
+PANIC = 0
+WORLD_ANOMALIES = 1000
+POPULATION = 7121416419
+
+#  FORMULAS
+TICK = 1
+SITE_SPACE = 25
+ANOMALY_PANIC = 0.002
+POP_GROWTH = 0.0000000037
+ANOMALY_RESEARCH = 0.005
+
 
 class Session:
 
@@ -10,11 +29,48 @@ class Session:
         self._day = 1
         self._month = 1
         self._year = 2015
-        self.funds = 500
+        self.world = World()
+        self.foundation = Foundation()
+
+    def acquire_anomaly(self):
+        self.foundation.anomalies += 1
+        self.world.anomalies -= 1
 
     def tick(self):
         self._advance_time()
         self._advance_date()
+        self._advance_game()
+
+    def _advance_game(self):
+        self.world.population += int(self.world.population * POP_GROWTH)
+        self.world.panic += int(self.world.anomalies * ANOMALY_PANIC)
+        self.foundation.research += int(self.foundation.researchers + (self.foundation.anomalies * ANOMALY_RESEARCH))
+
+    def _advance_time(self):
+        self._minutes += TICK
+        if self._minutes == 60:
+            self._minutes = 0
+            self._hours += 1
+            if self._hours == 24:
+                self._hours = 0
+                self._day += 1
+
+    def _advance_date(self):
+        if self._month in M_31:
+            if self._day == 31:
+                self._day = 1
+                self._month += 1
+        elif self._month in M_30:
+            if self._day == 30:
+                self._day = 1
+                self._month += 1
+        else:
+            if self._day == 28:
+                self._day = 1
+                self._month += 1
+        if self._month == 13:
+            self._month = 1
+            self._year += 1
 
     @property
     def time(self):
@@ -40,29 +96,24 @@ class Session:
             month = str(self._month)
         return day + "/" + month + "/" + str(self._year)
 
-    def _advance_time(self):
-        self._minutes += 1
-        if self._minutes == 60:
-            self._minutes = 0
-            self._hours += 1
-            if self._hours == 24:
-                self._hours = 0
-                self._day += 1
 
-    def _advance_date(self):
-        if self._month in M_31:
-            if self._day == 31:
-                self._day = 1
-                self._month += 1
-        elif self._month in M_30:
-            if self._day == 30:
-                self._day = 1
-                self._month += 1
-        else:
-            if self._day == 28:
-                self._day = 1
-                self._month += 1
-        if self._month == 13:
-            self._month = 1
-            self._year += 1
+class World:
+    def __init__(self):
+        self.population = POPULATION
+        self.panic = PANIC
+        self.anomalies = WORLD_ANOMALIES
 
+
+class Foundation:
+    def __init__(self):
+        self.funds = FUNDS
+        self.personnel = PERSONNEL
+        self.sites = SITES
+        self.anomalies = FOUNDATION_ANOMALIES
+        self.research = RESEARCH
+        self.agents = 0
+        self.researchers = 0
+
+    @property
+    def max_anomalies(self):
+        return self.sites * SITE_SPACE
